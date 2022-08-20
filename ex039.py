f7c5e4ec317dabbfae8969dfa9f69028c8f5417d
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-08-20 01:41:34 -0300

    if else de alistamento

---
+++ ex039.py
@@ -0,0 +1,15 @@
+from datetime import date
+atual = date.today().year
+nasc = int(input('Informa o ano de nascimento: '))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos em {}'.format(nasc,idade,atual))
+if idade == 18:
+    print('Você tem que se alistar')
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+elif idade > 18:
+    saldo = idade - 18
+    print('Você deveria ter se alistado há {} anos'.format(saldo))
+    ano = atual - saldo
+    print('Seu alistamento foi em {}'.format(ano))
