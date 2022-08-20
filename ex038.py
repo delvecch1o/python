055d446fdd9c7ca3414b5010686edf5dc347eb6c
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-08-20 00:30:34 -0300

    comparando numeros

---
+++ ex038.py
@@ -0,0 +1,8 @@
+num1 = int(input('Digite um numero inteiro : '))
+num2 = int(input('Digite um numero inteiro : '))
+if num1 > num2:
+    print('O PRIMEIRO numero é maior')
+elif num2 > num1:
+    print('O SEGUNDO numero é maior')
+elif num1 == num2:
+    print('Os dois valores são IGUAIS')
